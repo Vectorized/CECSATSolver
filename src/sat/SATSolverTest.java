f7bc3068c1ab9172795dd50542886616cfc13bf8
Liza Ng <liza@lnys.per.sg>
2013-11-08 21:50:48 +0800

    Add starting files

---
+++ src/sat/SATSolverTest.java
@@ -0,0 +1,64 @@
+package sat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sat.env.*;
+import sat.formula.*;
+
+
+public class SATSolverTest {
+    Literal a = PosLiteral.make("a");
+    Literal b = PosLiteral.make("b");
+    Literal c = PosLiteral.make("c");
+    Literal na = a.getNegation();
+    Literal nb = b.getNegation();
+    Literal nc = c.getNegation();
+
+
+
+	
+	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
+    
+	// TODO: put your test cases for SATSolver.solve here
+	
+    @Test
+    public void testSATSolver1(){
+    	// (a v b)
+    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
+    	assertTrue( "one of the literals should be set to true",
+    			Bool.TRUE == e.get(a.getVariable())  
+    			|| Bool.TRUE == e.get(b.getVariable())	);
+    	
+    	
+    }
+    
+    
+    @Test
+    public void testSATSolver2(){
+    	// (~a)
+    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
+    	assertEquals( Bool.FALSE, e.get(na.getVariable()));
+    	
+    }
+    
+    private static Formula makeFm(Clause... e) {
+        Formula f = new Formula();
+        for (Clause c : e) {
+            f = f.addClause(c);
+        }
+        return f;
+    }
+    
+    private static Clause makeCl(Literal... e) {
+        Clause c = new Clause();
+        for (Literal l : e) {
+            c = c.add(l);
+        }
+        return c;
+    }
+    
+    
+    
+}
